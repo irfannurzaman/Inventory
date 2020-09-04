fd95182ddb5e9bcc50117a5788501631eb3518a3
anitalim <anita.lim@hotmail.com>
2020-09-04 10:44:16 +0700

    irfan

--- src/app/modules/INV/PageINVRecipe.vue
+++ src/app/modules/INV/PageINVRecipe.vue
@@ -5,7 +5,7 @@
     </q-drawer>
     <div class="q-pa-lg">
       <div class="q-mb-md">
-        <q-btn @click="dialog = true" flat round class="q-mr-lg">
+        <q-btn @click="onClickDialog" flat round class="q-mr-lg">
           <img :src="require('~/app/icons/Icon-Add.svg')" height="30" />
         </q-btn>
         <q-btn flat round class="q-mr-lg">
@@ -21,9 +21,8 @@
         :data="data"
         :rows-per-page-options="[0]"
         :pagination.sync="pagination"
-        hide-bottom
+        :hide-bottom="hide_bottom"
         class="table-accounting-date"
-        @row-click="onRowClick"
       >
         <template #header-cell-fibukonto="props">
           <q-th :props="props" class="fixed-col left">{{ props.col.label }}</q-th>
@@ -56,28 +55,8 @@
       </STable>
     </div>
     <DialogChartOfAccounts
-      :dialog="dialog"
-      @saveData="saveData"
-      @cencel="dialog = false"
-      :selected="prepare"
-      :accountId="accountId"
+      :dialogRecipe="dialogRecipe"
     />
-    <q-dialog v-model="confirm" persistent>
-      <q-card>
-        <q-card-section class="row items-center">
-          <q-avatar icon="warning" color="primary" text-color="white" />
-          <span class="q-ml-sm">
-            Are you sure delete the stock article {{ 1101002 }} - Avocado
-            ?
-          </span>
-        </q-card-section>
-
-        <q-card-actions align="right">
-          <q-btn flat label="Cancel" color="primary" v-close-popup />
-          <q-btn @click="deleteData" flat label="Ok" color="primary" v-close-popup />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
   </div>
 </template>
 
@@ -96,117 +75,151 @@ export default defineComponent({
   setup(_, { root: { $api } }) {
     let charts;
     const state = reactive({
-      dialog: false,
-      confirm: false,
-      prepare: '',
-      data: [],
-      page: false,
-      accountId: '',
-      idDialog: '',
+      isFetching: false,
+      hide_bottom: false,
+      data: [] as any,
+      dialogRecipe: {
+        openDialog: false,
+        selectCatNo: []
+      },
     });
 
     // Helpers 
 
-        const NotifyCreate = (mess, col?) => Notify.create({
-              message: mess,
-              color: col,
-            });
+    const NotifyCreate = (mess, col?) => Notify
+      .create({
+          message: mess,
+          color: col,
+    });
+    const sortData = (charts, params) => {
+      if (params == 'bezeich1') {
+        return charts.sort((a, b) => {
+          const ab = a[params].toLowerCase()
+          const bc = b[params].toLowerCase()
+          if(ab < bc){
+            return -1
+          }
+        })
+      } else {
+        return charts
+        .sort((a, b) => a[params] - b[params])
+      } 
+      
+    }
+    const filterData = (charts, value, params) => {
+      return charts.filter(items => {
+        if (isNaN(value.inputan)) {            
+          return items.bezeich1.toLowerCase()
+          .includes(value.inputan.toLowerCase())
+        } else {
+          return items[params].toString()
+          .includes(value.inputan)
+        }})
+    }
+    const mapSelect = (GET_DATA) => {
+      const data = GET_DATA.recipe.recipe
+      return data.map(items => ({
+        label : `${items.katno} - ${items.bezeich}`,
+        value : items.katno
+      }))
+    }
 
     // Fetch API
 
     const FETCH_API = async (api, body?) => {
         const GET_DATA = await $api.inventory.FetchAPIINV(api, body)
         switch (api) {
-            case 'recipeListPrepare':
-                charts = DATA_RECIPE(GET_DATA)
-                state.data = charts
-                break;
-        
-            default:
-                break;
+          case 'recipeListPrepare':
+              charts = DATA_RECIPE(GET_DATA)
+              state.data = charts
+              if (charts.length !== 0) {
+                state.hide_bottom = true
+              }
+              break;
+          case 'recipeSelect':
+            const data = mapSelect(GET_DATA)
+            state.dialogRecipe.selectCatNo = data
+          default:
+              break;
         }
     }
 
+    // Function
+
     onMounted(async () => {
         FETCH_API('recipeListPrepare')
+        FETCH_API('recipeSelect')
     });
-    const saveData = () => {
-      state.dialog = false;
-    };
 
     const onSearch = (value) => {
-        if (value.group == '1') {
-            if (value.inputan == null || value.inputan == '') {
-                state.data = charts.sort((a, b) => {
-                    return a.artnrrezept - b.artnrrezept
-                })
-            } else {
-                if (!isNaN(value.inputan)) {                    
-                    state.data = charts.filter(items => {
-                        return items.artnrrezept.toString().includes(value.inputan)
-                    })
-                } else {
-                    NotifyCreate('this is not a number', 'red')
-                }
-            }
-        } else if(value.group == '2'){
-            if (value.inputan == null || value.inputan == '') {
-                state.data = charts.sort((a, b) => {
-                    const ab = a.bezeich.toLowerCase()
-                    const bc = b.bezeich.toLowerCase()
-                    if(ab < bc){
-                       return -1
-                    }
-                })
-            } else {
-                state.data = charts.filter(items => {
-                    return items.bezeich.toLowerCase().includes(value.inputan.toLowerCase())
-                })
-            }
+      if (value.group == '1') {
+        if (value.inputan == null || value.inputan == '') {
+          state.data = sortData(charts, 'artnrrezept')
         } else {
-            if (value.inputan == null || value.inputan == '') {
-                state.data = charts.sort((a, b) => {
-                    return a.kategorie - b.kategorie
-                })
+          if (!isNaN(value.inputan)) {                    
+            const x = filterData(charts, value, 'artnrrezept')
+            if (x.length !== 0) {
+              state.data = x
             } else {
-                if (!isNaN(value.inputan)) {  
-                state.data = charts.filter(items => {
-                    return items.kategorie.toString().includes(value.inputan)
-                })
-                } else {
-                    NotifyCreate('this is not a number', 'red')
-                }
+              NotifyCreate('Data Not Found', 'red')
             }
+          } else {
+            NotifyCreate('this is not a number', 'red')
+          }
         }
+      } else if(value.group == '2'){
+        if (value.inputan == null || value.inputan == '') {
+          state.data = sortData(charts, 'bezeich1')
+        } else {
+          const x = filterData(charts, value, 'bezeich1')
+          if(x.length !== 0){
+            state.data = x
+          } else {
+            NotifyCreate('Data Not Found', 'red')
+          }
+        }
+      } else {
+        if (value.inputan == null || value.inputan == '') {
+          state.data = sortData(charts, 'kategorie')
+        } else {
+          if (!isNaN(value.inputan)) {
+            const x = filterData(charts, value, 'kategorie')
+            if(x.length !== 0){
+              state.data = x
+            } else {
+              NotifyCreate('Data Not Found', 'red')
+            }
+          } else {
+            NotifyCreate('this is not a number', 'red')
+          }
+        }
+      }
     };
 
-    const editItem = (accountId) => {
-      state.accountId = accountId;
-      state.dialog = true;
-      state.idDialog = '2';
-    };
+    const onClickDialog = () => {
+      state.dialogRecipe.openDialog = true
+    }
 
-    const openDialog = () => {
-      state.dialog = true;
-      state.idDialog = '1';
-    };
+    // const editItem = (accountId) => {
+    //   state.accountId = accountId;
+    //   state.dialog = true;
+    //   state.idDialog = '2';
+    // };
 
-    const deleteData = async () => {
-      await Promise.all([
-        $api.inventory.apiRecipe('recipeListDelCheck', {
-          pvILanguage: 1,
-          ['t-h-rezept-artnrrezept']: 'Recipe Number',
-        }),
-      ]);
-    };
+
+    // const deleteData = async () => {
+    //   await Promise.all([
+    //     $api.inventory.FetchAPIINV('recipeListDelCheck', {
+    //       pvILanguage: 1,
+    //       ['t-h-rezept-artnrrezept']: 'Recipe Number',
+    //     }),
+    //   ]);
+    // };
 
     return {
       ...toRefs(state),
       onSearch,
-      saveData,
-      openDialog,
-      editItem,
-      deleteData,
+      onClickDialog,
       tableHeaders,
       pagination: {
         rowsPerPage: 0,
---
+++ src/app/modules/INV/components/DialogChildRecip.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-dialog v-model="dialogChildRecipe.openModalChild">
+    <q-toolbar>
+        <q-toolbar-title 
+        class="text-white text-weight-medium">
+           Recipe
+        </q-toolbar-title>
+    </q-toolbar>
+  </q-dialog>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  reactive,
+  toRefs,
+} from '@vue/composition-api';
+export default defineComponent({
+    props: {
+        dialogChildRecipe : {
+        type: Object, required: true}
+    },
+    setup(){
+      const state = reactive({
+            dialog: true
+      })
+    return {
+     ...toRefs(state),
+    }
+    }
+})
+</script>
+
+
+<style lang="scss" scoped>
+.q-toolbar {
+  background: $primary-grad;
+}
+</style>
